1c9646325b347c6a32cc25edbdd7177c6ac10156
Shahruk <mdshahruk2020@gmail.com>
2026-01-24 12:24:36 +0600

    solved remove duplicatres problem

---
+++ Arrays/remove_duplicates.py
@@ -0,0 +1,18 @@
+
+"""
+Time: O(n) where n is len of nums
+Space: O(1) use only constant extra space
+"""
+def brute_force_solve(nums):
+        res = 1
+        l = len(nums)
+        for x in range(1, l):
+            if nums[x-1] != nums[x]:
+                res += 1
+        return res 
+        
+    
+   
+
+nums = [3,3,3]
+print( brute_force_solve(nums))
