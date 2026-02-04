c92d61f82b38065c445c2744947e2cfa78ab3ce9
Shahruk <mdshahruk2020@gmail.com>
2026-02-04 10:25:08 +0600

    solve linkedlist_cycle problem

---
+++ Linked List/linkedlist_cycle.py
@@ -0,0 +1,31 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val 
+        self.next = next 
+
+def linkedlist(values, pos):
+    if not values:
+        return None
+    nodes = [ListNode(val) for val in values] 
+    for i in range(len(values) - 1):
+        nodes[i].next = nodes[i+1]
+    if pos > -1 and pos < len(values):
+        nodes[-1].next = nodes[pos]
+    return nodes[0]
+
+def hasCycle(nodes):
+    s= f = nodes
+    while f and f.next:
+        s = s.next
+        f = f.next.next
+        if s == f:
+            return True
+    return False
+
+
+values = [1,2,3,4]
+pos = 1 
+head = linkedlist(values, pos)
+
+print(hasCycle(head))
